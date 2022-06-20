35e000e2a4ebca0de05bde5d5cc53a9aad8d0a83
Pablo Riva <pabloarivaponce@gmail.com>
2022-06-20 17:02:39 -0400

    deleted lenguajeCompiladores-master folder

--- LenguajeCompiladores-master/consola.py
+++
@@ -1,9 +0,0 @@
-from unittest import result
-import ñ
-
-while True:
-    text = input("ñ >> ")
-    res, error = ñ.exe("<input>",text)
-
-    if error: print(error.como_str())
-    elif res: print(res)
